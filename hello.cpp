50c705d0989b0e0cf0271e1987acee55082fb79b
KanishkDadhich <lit2021007@iiitl.ac.in>
2022-09-30 12:21:34 +0530

    firsr c

---
+++ hello.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define vn vector<ll>v(n)
+#define arrn ll arr[n]
+#define arrni for(ll i=0;i<n;i++){cin>>arr[i];}
+#define vni for(ll i=0;i<n;i++){cin>>v[i];}
+#define endl "\n"
+#define tc  ll t;cin>>t;while(t--)
+#define pb emplace_back
+#define mapm map<ll,ll>m
+
+int main(){
+
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout<<"hello this is a random file"<<endl;
+
+
+
+
+
+
+
+
+}
